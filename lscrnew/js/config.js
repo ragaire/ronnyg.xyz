2cfda109ae51714b597025dac12f03a4ac62e702
Ronny Ragaire <99757203+ragaire@users.noreply.github.com>
2024-01-20 13:20:59 +0000

    Add files via upload

---
+++ lscrnew/js/config.js
@@ -0,0 +1,36 @@
+// ignore
+var Config = {};
+
+/**
+ * What should the text in the center of the screen be?
+ * if empty it will fill in your Server Name
+ */
+Config.title = "BENJI'S DOWNTOWN • DSC.GG/BENJISRP";
+
+/**
+ * Enable map text in the top left corner of the screen?
+ */
+Config.enableMap = true;
+
+/**
+ * Enable steamId text in the top right corner of the screen?
+ */
+Config.enableSteamID = true;
+
+
+/**
+ * How many miliseconds for each announcement?
+ * only works if enableAnnouncements = true
+ */
+Config.announcementLength = 3000;
+
+/**
+ * Image Filename
+ * DROP IMAGE IN "images" FOLDER
+ */
+Config.backgroundImage = "";
+
+/**
+ * Enable debug messages?
+ */
+Config.enableDebug = false;
