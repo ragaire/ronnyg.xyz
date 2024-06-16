7a152cf47afbebf97d8aae474e22874af1c3212a
Ronny Ragaire <99757203+ragaire@users.noreply.github.com>
2024-06-16 18:36:35 +0100

    Update config.js

--- lscrnew/js/config.js
+++ lscrnew/js/config.js
@@ -5,7 +5,7 @@ var Config = {};
  * What should the text in the center of the screen be?
  * if empty it will fill in your Server Name
  */
-Config.title = "BENJI'S DOWNTOWN • DSC.GG/BENJISRP";
+Config.title = "SUPER STUPID RETARD • DARKRP";
 
 /**
  * Enable map text in the top left corner of the screen?
